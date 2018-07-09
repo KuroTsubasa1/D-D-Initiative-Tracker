ae153576e71ac8a6f72364aad7d10f036f4c3fce
KuroTsubasa1 <1-IBI@web.de>
2018-07-09 09:33:42 +0200

    push preexisting files to github
    pushing the preexisting code to github

---
+++ Utils/utils.js
@@ -0,0 +1,36 @@
+module.exports ={
+    filewalker : function(dir, done)
+    {
+        let results = [];
+
+    fs.readdir(dir, function(err, list) {
+        if (err) return done(err);
+
+        var pending = list.length;
+
+        if (!pending) return done(null, results);
+
+        list.forEach(function(file){
+            file = path.resolve(dir, file);
+
+            fs.stat(file, function(err, stat){
+                // If directory, execute a recursive call
+                if (stat && stat.isDirectory()) {
+                    // Add directory to array [comment if you need to remove the directories from the array]
+                    results.push(file);
+
+                    module.exports.filewalker(file, function(err, res){
+                        results = results.concat(res);
+                        if (!--pending) done(null, results);
+                    });
+                } else {
+                    results.push(file);
+
+                    if (!--pending) done(null, results);
+                }
+            });
+        });
+    });
+    }
+
+};
